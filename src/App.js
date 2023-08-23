54f96fd50d18f4b5878a4e637f930bf86f599145
Kim_Nayeon <giant18s@naver.com>
2023-08-23 21:37:01 +0900

    4-react todolist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,86 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-    return <div>과제 파이팅 ~.~</div>;
+  const [inputValue, setInputValue] = useState("");
+  const [listContainerData, setListContainerData] = useState([]);
+  const [listDoneData, setListDoneData] = useState([]);
+
+  const addTask = () => {
+    if (inputValue === "") {
+      alert("Write something!!");
+    } else {
+      setListContainerData([...listContainerData, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const moveToDone = (index) => {
+    const task = listContainerData[index];
+    setListDoneData([...listDoneData, task]);
+    removeTask(index, false);
+  };
+
+  const moveToTodo = (index) => {
+    const task = listDoneData[index];
+    setListContainerData([...listContainerData, task]);
+    removeTask(index, true);
+  };
+
+  const removeTask = (index, isDone) => {
+    if (isDone) {
+      const updatedDoneData = listDoneData.filter((_, i) => i !== index);
+      setListDoneData(updatedDoneData);
+    } else {
+      const updatedContainerData = listContainerData.filter((_, i) => i !== index);
+      setListContainerData(updatedContainerData);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="todo-app">
+        <h2>
+          <img src="images/haerin.png" alt="" />
+          Things to do
+        </h2>
+        <div className="row">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Enter your to-do"
+          />
+          <button onClick={addTask}>+</button>
+        </div>
+        <p className="todo">
+          <img src="images/danielle.png" alt="" />
+          To do!
+        </p>
+        <ul className="list-container" id="list-container">
+          {listContainerData.map((task, index) => (
+            <li key={index}>
+              
+              <span className="del" onClick={() => removeTask(index, false)}>x</span>
+              <span className="move" onClick={() => moveToDone(index)}>{task}</span>
+            </li>
+          ))}
+        </ul>
+        <p className="done">
+          <img src="images/minji.png" alt="" />
+          Done!
+        </p>
+        <ul className="list-container" id="list-done">
+          {listDoneData.map((task, index) => (
+            <li key={index}>
+              <span className="donedel" onClick={() => removeTask(index, true)}>x</span>
+              <span className="move" onClick={() => moveToTodo(index)}>{task}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
